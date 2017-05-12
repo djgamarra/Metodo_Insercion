86170521c516be55dd316debc770968493642125
BlackSabbathX <josepadillainjema01@gmail.com>
2017-05-11 22:58:53 -0500

    Final

--- src/com/uninorte/sistemas53/Main.java
+++ src/com/uninorte/sistemas53/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
         Principal p = new Principal();
         p.setVisible(true);
+        //jose david padilla gamarra
     }
 }
 
